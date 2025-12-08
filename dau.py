5b991132710ddd5b1a2f192b907b1b44b789d9aa
dasha <hbs2t6s5x6@privaterelay.appleid.com>
2025-12-08 17:50:14 +0300

    Update dau.py

--- dau.py
+++ dau.py
@@ -4,6 +4,7 @@ import seaborn as sns
 
 df = pd.read_csv("dataset_telemetry.csv")
 
+#преобразуем в datetime формат
 df['timestamp'] = pd.to_datetime(df['timestamp'])
 df['date'] = df['timestamp'].dt.date
 df['hour'] = df['timestamp'].dt.hour
@@ -14,18 +15,24 @@ min_date = df['timestamp'].min()
 max_date = df['timestamp'].max()
 date_range = max_date - min_date
 
+#расчет dau
 dau = df.groupby('date')['userid'].nunique()
 
+#статистика пользователей
 unique_users = df['userid'].nunique()
 avg_dau = dau.mean()
 min_dau = dau.min()
 max_dau = dau.max()
 
+#создаем тепловую карту
 fig, ax = plt.subplots(figsize=(14, 8))
+
+#уникальных пользователей по каждому дню отдельно
 daily_unique_users = df.groupby(['date', 'day_of_week', 'hour'])['userid'].nunique().reset_index()
+#усредняем 
 pivot_data = daily_unique_users.groupby(['day_of_week', 'hour'])['userid'].mean().unstack(fill_value=0)
 
-
+#подписи
 weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 pivot_data = pivot_data.reindex(weekday)
 days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
@@ -45,5 +52,6 @@ ax.set_title(
     fontsize=14,)
 
 plt.tight_layout()
+
 
 plt.show()
